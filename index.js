2192c834d9187625778fb2284c5ee989f3a15f68
Joey Hendricks <joeysguitartabs@yahoo.com>
2020-04-19 03:02:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function takeANumber(currLine, newCustName) {
+	katzDeliLine.push(newCustName);
 	if(katzDeliLine.length === 0){
 		return `Welcome, ${newCustName}. You are number 1 in line.`;
 	} else if( katzDeliLine.length !==0){
-		return `Welcome ${newCustName}. You are number ${katzDeliLine.length+1} in line.`;
+		return `Welcome ${newCustName}. You are number ${katzDeliLine.length} in line.`;
 	}
 }
