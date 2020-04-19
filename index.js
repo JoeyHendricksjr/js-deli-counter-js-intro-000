8ddafd99892c6b81dcbfea28aaf417455450852b
Joey Hendricks <joeysguitartabs@yahoo.com>
2020-04-19 03:18:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function takeANumber(currLine, newCustName) {
 		return `Welcome, ${newCustName}. You are number ${currLine.length} in line.`;
 	}
 }
+function nowServing(arr){
+	if(arr.length !== 0){
+		//let firstInLine = arr[0];
+		//arr.slice(1,1);
+		return `Now serving ${arr[0]}`;
+		// return arr;
+	} else {
+		return 'There is nobody waiting to be served.';
+	}
+}
