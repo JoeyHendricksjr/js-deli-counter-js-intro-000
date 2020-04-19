b70226352b3f11168bf2598dd095317e063bfdcc
Joey Hendricks <joeysguitartabs@yahoo.com>
2020-04-19 04:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ function nowServing(arr){
 		return 'There is nobody waiting to be served!';
 	}
 }
+function currentLine(line){
+	var curLine = [];
+	if(line.length === 0){
+		return 'The line is currently empty.';
+	} else {
+	for(let i = 0; i < line.length; i++){
+		curLine += (i + 1) + '. ' + line[i] + ', ';
+		//console.log(curLine);
+	}
+ }
+ return curLine;
+}
